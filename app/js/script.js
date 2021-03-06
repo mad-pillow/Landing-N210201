e2ff6e9de851e1d79d9bf407cfcb3c8c45635106
Mad Pillow <dmtr.schv@gmail.com>
2021-03-07 01:39:52 +0200

    feat: rework responsivness of links and slides structure

--- app/js/script.js
+++ app/js/script.js
@@ -3,45 +3,66 @@ let sliderRightBtn = document.getElementsByClassName('latest-articles__right-btn
 let sliderContainer = document.getElementsByClassName('latest-articles__slider-container');
 let sliderList = document.getElementsByClassName('latest-articles__list');
 let sliderItems = document.getElementsByClassName('latest-articles__item');
-let i = 0;
-let l = 0;
-let wc = 0;
-let ml = 0;
-let wi = 0;
+let itemPosition = 0;
+let sliderLength = 0;
+let widthOfSlider = 0;
+let sliderItemMarginLeft = 0;
+let widthOfSliderItem = 0;
 
-let setSlider = function () {
-   wc = parseInt(getComputedStyle(sliderContainer[0]).getPropertyValue('width').replace('px', ''));
-   wi = parseInt(getComputedStyle(sliderItems[0]).getPropertyValue('width').replace('px', ''));
-   ml = parseInt(getComputedStyle(sliderItems[1]).getPropertyValue('margin-left').replace('px', ''));
-   l = sliderItems.length - parseInt(wc / wi);
-};
+function setSlider() {
+  widthOfSlider = parseInt(getComputedStyle(sliderContainer[0]).getPropertyValue('width').replace('px', ''));
+  widthOfSliderItem = parseInt(getComputedStyle(sliderItems[0]).getPropertyValue('width').replace('px', ''));
+  sliderItemMarginLeft = parseInt(getComputedStyle(sliderItems[1]).getPropertyValue('margin-left').replace('px', ''));
+  sliderLength = sliderItems.length - parseInt(widthOfSlider / widthOfSliderItem);
+}
 
 setSlider();
 
 window.addEventListener('resize', setSlider);
 
-let sliderToLeft = function () {
-   if (i < 0) {
-      i++;
-      sliderList[0].style.left = (wi + ml) * i + 'px';
-   }
-};
+function sliderToLeft() {
+  if (itemPosition < 0) {
+    itemPosition++;
+    sliderList[0].style.left = (widthOfSliderItem + sliderItemMarginLeft) * itemPosition + 'px';
+  }
+}
 
-let sliderToRight = function () {
-   i--;
-   if (i >= -1 * l) {
-      sliderList[0].style.left = (wi + ml) * i + 'px';
-   } else {
-      sliderList[0].style.left = 0 + 'px';
-      i = 0;
-   }
-};
+function sliderToRight() {
+  itemPosition--;
+  if (itemPosition >= -1 * sliderLength) {
+    sliderList[0].style.left = (widthOfSliderItem + sliderItemMarginLeft) * itemPosition + 'px';
+  } else {
+    sliderList[0].style.left = 0 + 'px';
+    itemPosition = 0;
+  }
+}
 
-sliderLeftBtn[0].addEventListener('click', sliderToLeft);
+sliderLeftBtn[0].addEventListener('click', () => {
+  int = clearInterval(int);
+  sliderToLeft();
+});
 
-sliderRightBtn[0].addEventListener('click', function () {
-   clearInterval(int);
-   sliderToRight();
+sliderRightBtn[0].addEventListener('click', () => {
+  int = clearInterval(int);
+  sliderToRight();
 });
 
 let int = setInterval(sliderToRight, 3000);
+
+window.addEventListener('scroll', () => {
+  if (typeof int == 'undefined') {
+    int = setInterval(sliderToRight, 3000);
+  }
+});
+
+let touchOnX = null;
+
+function touchOn(e) {
+  touchOnX = e.touches[0].clientX;
+  console.log(touchOnX);
+}
+
+sliderItems[0].addEventListener('touch', e => {
+  e.preventDefault();
+  touchOn(e);
+});
